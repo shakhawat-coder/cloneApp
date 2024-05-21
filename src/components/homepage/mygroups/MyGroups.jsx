fb2e0a2a8c2094d07ec801dde35ff8734993b824
shakhawat <shakhawathossen188@gmail.com>
2024-05-21 16:08:23 +0600

    Home page design done

--- src/components/homepage/mygroups/MyGroups.jsx
+++ src/components/homepage/mygroups/MyGroups.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { FaEllipsisVertical } from "react-icons/fa6";
-import Search from "../../common/Search";
 import profilePicture from "../../../assets/Home/profile.png";
 import profilePictur2 from "../../../assets/Home/profile.png";
 import profilePicture3 from "../../../assets/Home/profile.png";
 import profilePicture4 from "../../../assets/Home/profile.png";
-const GroupList = () => {
-  const users = [
+const Mygroups = () => {
+  const friends = [
     {
       id: 1,
       image: profilePicture,
-      title: "Friends Reunion",
-      description: "Hi Guys, Wassup!",
-      button: "Join",
+      title: "Raghav",
+      description: "Dinner?",
+      time: "Today, 8:56pm",
+      active: true,
     },
 
     {
@@ -20,7 +20,8 @@ const GroupList = () => {
       image: profilePictur2,
       title: "Friends Forever",
       description: "Good to see you.",
-      button: "Join",
+      time: "Today, 8:56pm",
+      active: false,
     },
 
     {
@@ -28,7 +29,8 @@ const GroupList = () => {
       image: profilePicture3,
       title: "Crazy Cousins",
       description: "What plans today?",
-      button: "Join",
+      time: "Today, 8:56pm",
+      active: true,
     },
 
     {
@@ -36,31 +38,29 @@ const GroupList = () => {
       image: profilePicture4,
       title: "Bechelor Hub",
       description: "Lets Do Party",
-      button: "join",
+      time: "Today, 8:56pm",
+      active: false,
     },
   ];
   return (
     <>
       <div className="">
-        <div className="">
-          <Search className={"w-[427px]"} />
-        </div>
-        <div className=" left-0 right-0 my-5 flex items-center justify-between bg-white px-3">
+        <div className=" left-0 right-0 my-5   flex items-center justify-between bg-white px-3">
           <h1 className="font-Poppins text-custom-black text-xl font-semibold">
-            Groups List
+            My Groups
           </h1>
           <span>
             <FaEllipsisVertical className="text-btn-color text-2xl" />
           </span>
         </div>
-        <div className="scrollbar-thin scrollbar-thumb-sky-700 scrollbar-track-gray-400 relative mt-10  h-[362px] w-[427px] overflow-y-scroll rounded-xl shadow-xl">
+        <div className="scrollbar-thin scrollbar-thumb-sky-700 scrollbar-track-gray-400 relative h-[442px] w-[427px] overflow-y-scroll rounded-xl shadow-xl">
           <div className=" divide-y-[1px] divide-gray-200">
-            {users?.map((item, index) => (
+            {friends?.map((item, index) => (
               <div
-                key={index}
+                key={item.id}
                 className="flex items-center justify-between px-7 py-5"
               >
-                <div className="h-[70px] w-[70px] cursor-pointer">
+                <div className="relative h-[70px] w-[70px] cursor-pointer">
                   <picture>
                     <img
                       src={item.image}
@@ -68,9 +68,15 @@ const GroupList = () => {
                       className="s-full h-full rounded-full object-cover shadow-lg"
                     />
                   </picture>
+                  {item.active && (
+                    <span className="absolute bottom-2 right-0 flex h-3 w-3">
+                      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-400 opacity-75"></span>
+                      <span className="relative inline-flex h-3 w-3 rounded-full bg-green-500"></span>
+                    </span>
+                  )}
                 </div>
 
-                <div className="flex w-[60%] flex-col items-center justify-center text-wrap   ">
+                <div className="flex w-[40%] flex-col items-start justify-center text-wrap   ">
                   <h1 className="font-Poppins text-custom-black text-lg font-semibold">
                     {item.title}
                   </h1>
@@ -80,8 +86,8 @@ const GroupList = () => {
                 </div>
 
                 <div>
-                  <button className="font-Poppins rounded-lg bg-blue px-5 py-1 text-xl font-semibold text-white">
-                    {item.button}
+                  <button className="font-Poppins text-xs font-medium  text-black opacity-50 ">
+                    {item.time}
                   </button>
                 </div>
               </div>
@@ -93,4 +99,4 @@ const GroupList = () => {
   );
 };
 
-export default GroupList;
+export default Mygroups;
--- src/pages/HomepageContent.jsx
+++ src/pages/HomepageContent.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import GroupList from "../components/homepage/grouplist/GroupList";
+import Friends from "../components/homepage/friends/Friends";
+import UserList from "../components/homepage/userList/UserList";
+import FriendRequest from "../components/homepage/friendRequest/FriendRequest";
+import MyGroups from "../components/homepage/mygroups/MyGroups";
+import BlockedUser from "../components/homepage/blockedUser/BlockedUser";
 
 const HomepageContent = () => {
   return (
     <>
-      <div className="">
+      <div className="flex flex-wrap justify-between gap-y-5  ">
         <GroupList />
+        <Friends />
+        <UserList />
+        <FriendRequest />
+        <MyGroups />
+        <BlockedUser />
       </div>
     </>
   );
